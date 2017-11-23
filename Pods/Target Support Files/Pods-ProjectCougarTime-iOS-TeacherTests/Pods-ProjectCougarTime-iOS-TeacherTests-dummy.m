b7da759eee4d807adfebb947cabd2ed218a75b45
ApolloZhu <public-apollonian@outlook.com>
2017-11-23 11:59:53 -0500

    Add Google Sign In SDK

---
+++ Pods/Target Support Files/Pods-ProjectCougarTime-iOS-TeacherTests/Pods-ProjectCougarTime-iOS-TeacherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectCougarTime_iOS_TeacherTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectCougarTime_iOS_TeacherTests
+@end
